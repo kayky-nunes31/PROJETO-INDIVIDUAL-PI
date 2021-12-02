4a2908cc4bbcaa9565c92b262f893b0ec163985e
kayky-nunes31 <kayky.nunes@bandtec.com.br>
2021-12-02 15:31:03 -0300

    adicionando gradiente e efeitos

--- api-site/src/database/config.js
+++ api-site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "admin",
     database: "ledessin",
-    password: "123456",
+    password: "D1g1t@lschool",
 };
 
 function executar(instrucao) {
